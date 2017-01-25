57acf44d60662031f38faf9fa9367d44a5e1fc22
Anton Golomazov <anton.golomazov@devim.team>
2017-01-25 16:24:29 +0300

    Add login component

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container login">
+    <h1 class="header-page">Login page</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'login',
+    data(){
+      return {
+
+      }
+    }
+  }
+</script>
